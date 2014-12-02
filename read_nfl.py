84215ba3557ca4b1a7f5c7cd891040677e55a451
Hugh Cunningham <hughec@dn0a232088.sunet>
2014-12-02 13:25:41 -0800

    updated read_nfl with a function to read team defensive stats from team files.  also made changes to the read_player_data function to create a dictionary for player stats, and minor changes to the extract_game_stats function to include stats for carries, receptions, etc.

--- read_nfl.py
+++ read_nfl.py
@@ -1,17 +1,47 @@
 import csv
 import os
+from collections import Counter
+
+league = ['SEA', 'GB', 'ATL', 'NO', 'BAL', 'CIN', 'CHI', 'BUF', 'HOU', 
+	'WAS', 'KC', 'TEN', 'MIA', 'NE', 'NYJ', 'OAK', 'PHI', 'JAC', 'PIT', 
+	'CLE', 'STL', 'MIN', 'DAL', 'SF', 'TB', 'CAR', 'DEN', 'IND', 'DET', 
+	'NYG', 'ARI', 'SD']
+
+def read_team_data(seasons=range(2010,2014), weeks=range(1,18), teams=league):
+	team_defense = {}
+	for team in teams:
+		team_defense[team] = {}
+		for season in seasons:
+			team_defense[team][season] = {}
+			for week in weeks:
+				filename = "../nfl_team_data/" + team + '_' + str(season) + '_' + str(week) + '.csv'
+				# no game in this week
+				if not os.path.exists(filename):
+					team_defense[team][season][week] = None
+					continue
+				defense_stats = Counter()
+				print filename
+				with open(filename) as game:
+					gamereader = csv.DictReader(game)
+					for player in gamereader:
+						game_stats = Counter(extract_game_stats(player))
+						defense_stats = defense_stats + game_stats
+				team_defense[team][season][week] = defense_stats
+	return team_defense
+
 
 # seasons and weeks are lists of seasons and weeks to read data for.
 def read_player_data(seasons=range(2010,2014), weeks=range(1,18)):
 	# dictionary player_scores[player_id][year][week] = FF score for player in that week of that year (None if they didnt play)
-        # dic player_info[player_id] to (name, team, pos)
+	# dic player_info[player_id] to (name, team, pos)
 	player_scores = {}
-    player_info = {}
+	player_info = {}
+	player_stats = {}
 	for season in seasons:
 		for week in weeks:
 			filename = '../nfl_game_data/' + str(season) + '_' + str(week) + '.csv'
-                        if not os.path.exists(filename):
-                                continue
+			if not os.path.exists(filename): 
+				continue
 			with open(filename) as games:
 				gamesreader = csv.DictReader(games)
 				for player in gamesreader:
@@ -22,8 +52,9 @@ def read_player_data(seasons=range(2010,2014), weeks=range(1,18)):
 					fantasy_score = yahoo_fantasy_score(game_stats)
 					if player_id in player_scores:
 						player_scores[player_id][season][week] = fantasy_score
-						player_totals[player_id] += fantasy_score
+						player_stats[player_id][season][week] = game_stats
 					else:
+						# initialize empty dict for seasons, weeks
 						season_dict = {}
 						for season_year in seasons:
 							week_dict = {}
@@ -31,21 +62,35 @@ def read_player_data(seasons=range(2010,2014), weeks=range(1,18)):
 								week_dict[week_number] = None
 							season_dict[season_year] = week_dict
 						player_scores[player_id] = season_dict
-						player_info[player_id] = (player['name'], player['team'], player['pos']) 						
+						player_stats[player_id] = season_dict
+                        player_info[player_id] = (player['name'], player['team'], player['pos']) 						
 	return player_info, player_scores
+
+# some games are missing stats like puntret_tds, this avoid errors associated with missing stats
+def safe_stat_read(player, stat):
+	if stat in player:
+		return float(player[stat] or '0')
+	else:
+		return 0
 
 # given a player dictionary object for a game, returns a new dictionary containing only the necessary stats.
 def extract_game_stats(player):
 	stats = {}
+	stats['home'] = (player['home'] == 'yes')
+	stats['passing_att'] = float(player['passing_att'] or '0')
+	stats['passing_cmp'] = float(player['passing_cmp'] or '0')
 	stats['passing_yds'] = float(player['passing_yds'] or '0')
 	stats['passing_tds'] = float(player['passing_tds'] or '0')
 	stats['passing_ints'] = float(player['passing_ints'] or '0')
+	stats['rushing_att'] = float(player['rushing_att'] or '0')
 	stats['rushing_yds'] = float(player['rushing_yds'] or '0')
 	stats['rushing_tds'] = float(player['rushing_tds'] or '0')
+	stats['receiving_rec'] = float(player['receiving_rec'] or '0')
 	stats['receiving_yds'] = float(player['receiving_yds'] or '0')
 	stats['receiving_tds'] = float(player['receiving_tds'] or '0')
-	stats['ret_tds'] = float(player['puntret_tds'] or '0') + float(player['kickret_tds'] or '0')
-	stats['fumbles_lost'] = float(player['fumbles_lost'] or '0')
+	stats['ret_tds'] = safe_stat_read(player, 'puntret_tds')
+	stats['ret_tds'] += safe_stat_read(player, 'kickret_tds')
+	stats['fumbles_lost'] = safe_stat_read(player, 'fumbles_lost')
 	stats['twoptm'] = float(player['passing_twoptm'] or '0') + float(player['rushing_twoptm'] or '0') + float(player['receiving_twoptm'] or '0')
 	return stats
 
